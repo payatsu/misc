96a65312579fa1de1bb4add1aafc96deb6aa35da
payatsu <6873447+payatsu@users.noreply.github.com>
2015-07-06 18:07:21 +0900

    Modified ip address.

--- python/udp.py
+++ python/udp.py
@@ -1,6 +1,6 @@
 import socket
 
-host = '255.255.255.255' # '192.168.1.6'
+host = '192.168.10.255'
 port = 4000 # shall confirm the media players accept the port.
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.sendto('abc'.encode('utf-8'), (host, port))
