82f1fe87b552973a8ee2dcb165d4511d34cea3ec
payatsu <6873447+payatsu@users.noreply.github.com>
2015-07-14 23:13:24 +0900

    17_years_old.py - 今日の年齢を17歳基準で計算する。

---
+++ python/17_years_old.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+import argparse
+import datetime
+
+parser = argparse.ArgumentParser(description = 'Calculate your age.')
+parser.add_argument('-y', '--year', default = 1964, type = int)
+parser.add_argument('-m', '--month', default = 9, type = int)
+parser.add_argument('-d', '--day', default = 25, type = int)
+args = parser.parse_args()
+
+delta = datetime.date.today() - datetime.date(args.year, args.month, args.day)
+
+years = delta.days // 365
+months = 0
+days = delta.days % 365
+if 17 < years:
+	months = (years - 17) * 12
+	years = 17
+
+print('You are {years} years, {months} months, {days} days old.'.format(years = years, months = months, days = days))
