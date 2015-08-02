e1cc320be7ba9d85b579fd547d1d91fa1f996a2a
payatsu <6873447+payatsu@users.noreply.github.com>
2015-08-02 13:38:54 +0900

    ADT7410用のpython script.

---
+++ python/adt7410.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import smbus
+
+i2c = smbus.SMBus(1)
+address = 0x48
+
+block = i2c.read_i2c_block_data(address, 0x00, 12)
+temp = (block[0] << 8 | block[1]) >> 3
+if temp >= 4096:
+	temp -= 8192
+print(temp/16.0)
