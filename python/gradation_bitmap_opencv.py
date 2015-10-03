02dec1adf6dd27a6aa72d3452bdd87890d438c89
payatsu <6873447+payatsu@users.noreply.github.com>
2015-10-03 22:25:52 +0900

    openCVを使用してグラデーション画像を生成するPythonスクリプト。

---
+++ python/gradation_bitmap_opencv.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+import cv2, numpy
+
+height = 1080
+max_grad = 255
+img = numpy.zeros((height, max_grad+1, 3))
+
+for i in range(height):
+	for j in range(max_grad+1):
+		img[i, j, 0] = j
+
+cv2.imwrite('/tmp/hoge.bmp', img)
