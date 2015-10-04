a886d7e7719d7423df2fbe541b39ac7c94f15aa6
payatsu <6873447+payatsu@users.noreply.github.com>
2015-10-04 09:22:04 +0900

    生成した画像の保存場所をカレントディレクトリに変更した(マルチプラットフォーム対応)。

--- python/gradation_bitmap_opencv.py
+++ python/gradation_bitmap_opencv.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-import cv2, numpy
+import cv2, numpy, os
 
 height = 1080
 max_grad = 255
@@ -10,4 +10,4 @@ for i in range(height):
 	for j in range(max_grad+1):
 		img[i, j, 0] = j
 
-cv2.imwrite('/tmp/hoge.bmp', img)
+cv2.imwrite(os.getcwd() + os.sep + 'gradation.bmp', img)
