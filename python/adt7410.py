bc586fdf62ea9c7b91d59647d90bd6456027d724
payatsu <6873447+payatsu@users.noreply.github.com>
2015-10-04 10:07:49 +0900

    コードを短くした。

--- python/adt7410.py
+++ python/adt7410.py
@@ -3,10 +3,9 @@
 
 import smbus
 
-i2c = smbus.SMBus(1)
 address = 0x48
 
-block = i2c.read_i2c_block_data(address, 0x00, 12)
+block = smbus.SMBus(1).read_i2c_block_data(address, 0x00, 12)
 temp = (block[0] << 8 | block[1]) >> 3
 if temp >= 4096:
 	temp -= 8192
