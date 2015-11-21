af1fb462233c0f8879e9a39ac7b9bab8282ec662
payatsu <6873447+payatsu@users.noreply.github.com>
2015-11-22 00:09:27 +0900

    libbpg.shメンテナンスした。不完全だけど。

--- sh/08_libbpg.sh
+++ sh/08_libbpg.sh
@@ -4,16 +4,16 @@
 
 # install libpng 1.6 or later, at first.
 
-SUDO='sudo '
-LIBPNG_VERSION=libpng-1.6.16
-LIBBPG_VERSION=libbpg-0.9.4
+LIBPNG_VERSION=libpng-1.6.19
+LIBBPG_VERSION=libbpg-0.9.6
 
-${SUDO}mkdir -p /usr/local/src/libpng
+apt-get install --yes cmake libsdl1.2-dev libsdl-image1.2-dev # libsdl2-dev
+
+mkdir -p /usr/local/src/libpng
 cd /usr/local/src/libpng
 
 if [ ! -d ${LIBPNG_VERSION} ]; then
-	if ! wget -O- ftp://ftp.simplesystems.org/pub/libpng/png/src/libpng16/${LIBPNG_VERSION}.tar.gz | ${SUDO}tar xzf ;
-	then
+	if ! wget -O- ftp://ftp.simplesystems.org/pub/libpng/png/src/libpng16/${LIBPNG_VERSION}.tar.gz | tar xzf -; then
 		echo \`wget\' failed. 1>&2
 		exit 1
 	fi
@@ -21,29 +21,27 @@ fi
 
 cd ${LIBPNG_VERSION}
 if [ ! -f Makefile ]; then
-	if ! ${SUDO}./configure; then
+	if ! ./configure; then
 		echo \`./configure\' failed. 1>&2
 		exit 1
 	fi
 fi
 
-if ! ${SUDO}make check; then
+if ! make check; then
 	echo \`make\' failed. 1>&2
 	exit 1
 fi
 
-if ! ${SUDO}make install; then
+if ! make install; then
 	echo \`make install\' failed. 1>&2
 	exit 1
 fi
 
-#
-
-${SUDO}mkdir -p /usr/local/src/libbpg
+mkdir -p /usr/local/src/libbpg
 cd /usr/local/src/libbpg
 
 if [ ! -d ${LIBBPG_VERSION} ]; then
-	if ! wget -O- http://bellard.org/bpg/${LIBBPG_VERSION}.tar.gz | ${SUDO}tar xzf -; then
+	if ! wget -O- http://bellard.org/bpg/${LIBBPG_VERSION}.tar.gz | tar xzf -; then
 		echo \`wget\' failed. 1>&2
 		exit 1
 	fi
