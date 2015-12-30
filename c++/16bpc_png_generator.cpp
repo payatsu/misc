71061d05dcdae76943cdf4683335d65a1a3da8d4
payatsu <6873447+payatsu@users.noreply.github.com>
2015-12-31 02:47:07 +0900

    16bit PNG画像生成プログラムの再利用性を高めた。

--- c++/16bpc_png_generator.cpp
+++ c++/16bpc_png_generator.cpp
@@ -15,7 +15,6 @@
 #define PNG_NO_SETJMP
 
 #include <cstdio>
-#include <functional>
 #include <memory>
 #include <png.h>
 
@@ -47,11 +46,26 @@ struct Image{
 	Row operator[](int row){return Row(block_ + row*width*pixelsize);}
 };
 
-int main(int argc, char* argv[])
+void gradation(Image image)
 {
-	std::unique_ptr<FILE, decltype(&std::fclose)> fp(std::fopen("16bpc_img.png", "wb"), std::fclose);
+	for(png_uint_16 row=0; row<height; ++row){
+		for(png_uint_16 column=0; column<width; ++column){
+			if(row<=height/3){
+				image[row][column] = {column*0xffff/width, 0x00, 0x00};
+			}else if(row<=height/3*2){
+				image[row][column] = {0x00, 0xffff-column*0xffff/width, 0x00};
+			}else{
+				image[row][column] = {0x00, 0x00, column*0xffff/width};
+			}
+		}
+	}
+}
+
+int generate_16bpc_png(std::string prog_name, std::string output_filename, std::function<void(Image)> generator)
+{
+	std::unique_ptr<FILE, decltype(&std::fclose)> fp(std::fopen(output_filename.c_str(), "wb"), std::fclose);
 	if(!fp){
-		std::perror(argv[0]);
+		std::perror(prog_name.c_str());
 		return -1;
 	}
 	png_structp png_ptr = png_create_write_struct(PNG_LIBPNG_VER_STRING, nullptr, nullptr, nullptr);
@@ -66,24 +80,18 @@ int main(int argc, char* argv[])
 	png_init_io(png_ptr, fp.get());
 	png_set_IHDR(png_ptr, info_ptr, width, height, bitdepth, colortype, PNG_INTERLACE_NONE, PNG_COMPRESSION_TYPE_DEFAULT, PNG_FILTER_TYPE_DEFAULT);
 	std::unique_ptr<png_byte[]> block(new png_byte[height*width*pixelsize]);
-
-	/* Edit from here, */
-
-	Image image(block.get());
-	for(int row=0; row<height; ++row){
-		for(int column=0; column<width; ++column){
-			image[row][column] = {column*0xffff/width, 0x00, 0x00};
-		}
-	}
-
-	/* to here */
-
 	std::unique_ptr<png_bytep[]> row_ptrs(new png_bytep[height]);
 	for(int i=0; i<height; ++i){
 		row_ptrs[i] = block.get() + i*width*pixelsize;
 	}
 	png_set_rows(png_ptr, info_ptr, row_ptrs.get());
+	generator(Image(block.get()));
 	png_write_png(png_ptr, info_ptr, PNG_TRANSFORM_IDENTITY, nullptr);
 	png_destroy_write_struct(&png_ptr, &info_ptr);
 	return 0;
+}
+
+int main(int argc, char* argv[])
+{
+	return generate_16bpc_png(argv[0], "16bpc_img.png", gradation);
 }
