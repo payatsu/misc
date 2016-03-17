c6566a64221ea317b0cc6dd5298263d962504b09
payatsu <6873447+payatsu@users.noreply.github.com>
2016-03-18 00:56:41 +0900

    i2cのC++サンプルを新規作成。

---
+++ c++/i2c/i2c.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <fcntl.h>
+#include <linux/i2c.h>
+#include <linux/i2c-dev.h>
+#include <sys/ioctl.h>
+#include <unistd.h>
+
+int main(void)
+{
+	int fd = open("/dev/i2c-1", O_RDWR);
+	if(fd == -1){
+		perror("open()");
+		return -1;
+	}
+
+	if(ioctl(fd, I2C_SLAVE, 0x48) < 0){
+		perror("ioctl");
+		return -1;
+	}
+	const int size = 12;
+	uint8_t buf[size] = {};
+	buf[0] = 0x48;
+	if(write(fd, buf, 1) != 1){
+		perror("write");
+		return -1;
+	}
+	if(read(fd, buf, size) != size){
+		perror("read");
+		return -1;
+	}
+
+	close(fd);
+	return 0;
+
+	/*************************
+	i2c_msg msg = {
+		.addr  = 0x48,
+		.flags = I2C_M_RD,
+		.len   = size,
+		.buf   = buf,
+	};
+	i2c_rdwr_ioctl_data data = {
+		.msgs  = &msg,
+		.nmsgs = 1,
+	};
+
+	if(ioctl(fd, I2C_RDWR, &data) == -1){
+		perror("ioctl()");
+		close(fd);
+		return -1;
+	}
+	close(fd);
+	*************************/
+}
