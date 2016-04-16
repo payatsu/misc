5a775344b74b48d611d8a339e98c18bfc4ae25b8
payatsu <6873447+payatsu@users.noreply.github.com>
2016-04-16 14:24:44 +0900

    16bpcgenのディレクトリ構造変更。

---
+++ c++/16bpcgen/v2/include/getopt.hpp
@@ -0,0 +1,26 @@
+#ifndef _16BPCGEN_GETOPT_HPP_
+#define _16BPCGEN_GETOPT_HPP_
+
+#include <algorithm>
+#include <cstring>
+#include <map>
+#include <string>
+
+typedef std::map<std::string, std::string> Store;
+bool is_equal(char c){return c == '=';}
+
+inline Store getopt(int argc, char* argv[])
+{
+	Store store;
+	for(int i = 1; i < argc; ++i){
+		if(std::count_if(argv[i], argv[i] + std::strlen(argv[i]), is_equal) == 1){
+			const std::string::size_type idx = std::string(argv[i]).find('=');
+			const std::string key(argv[i], idx);
+			const std::string value(argv[i] + idx + 1);
+			store[key] = value;
+		}
+	}
+	return store;
+}
+
+#endif
