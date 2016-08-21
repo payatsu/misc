3aae9e6c5ed84686775f4b711911537e00e906ba
payatsu <6873447+payatsu@users.noreply.github.com>
2016-08-21 21:48:36 +0900

    sysopen使わないようにした。

--- ruby/adt7410.rb
+++ ruby/adt7410.rb
@@ -1,11 +1,10 @@
 #!/usr/bin/ruby
 require 'fcntl'
 
-fd = IO.sysopen '/dev/i2c-1', Fcntl::O_RDWR
-IO.open fd, IO::RDWR do |io|
-	io.ioctl(0x0703, 0x48)
-	io.syswrite 0x48.chr
-	buf = io.sysread 12
+File.open '/dev/i2c-1', IO::RDWR do |f|
+	f.ioctl(0x0703, 0x48)
+	f.syswrite 0x48.chr
+	buf = f.sysread 12
 	temp = (buf[0].ord << 8 | buf[1].ord) >> 3
 	temp -= 8192 if 4096 <= temp
 	printf "%f\n", temp/16.0
