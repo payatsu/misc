e843fdef94a8e5968c5a39e3720d96f7938a4043
payatsu <6873447+payatsu@users.noreply.github.com>
2016-08-21 20:57:00 +0900

    adt7410.rbが動くこうにした。

--- ruby/adt7410.rb
+++ ruby/adt7410.rb
@@ -1,17 +1,12 @@
 #!/usr/bin/ruby
-# -*- condig: utf-8 -*-
+require 'fcntl'
 
-dev = '/dev/i2c-1'
-
-if __FILE__ == $0
-	fd = IO.sysopen dev, IO::RDWR
-	io = IO.new fd, IO::RDWR
+fd = IO.sysopen '/dev/i2c-1', Fcntl::O_RDWR
+IO.open fd, IO::RDWR{|io|
 	io.ioctl(0x0703, 0x48)
-	buf = Array.new
-	buf << 0x48
-	io.write buf
-	io.read buf
-	io.close
-
-	puts buf[0], buf[1]
-end
+	io.syswrite 0x48.chr
+	buf = io.sysread 12
+	temp = (buf[0].ord << 8 | buf[1].ord) >> 3
+	temp -= 8192 if 4096 <= temp
+	printf "%f\n", temp/16.0
+}
