195d172fe36d1ff8b5578b1690a1d657d147a475
payatsu <6873447+payatsu@users.noreply.github.com>
2016-08-21 21:31:26 +0900

    {}からdo end方式に書き換えた。

--- ruby/adt7410.rb
+++ ruby/adt7410.rb
@@ -2,11 +2,11 @@
 require 'fcntl'
 
 fd = IO.sysopen '/dev/i2c-1', Fcntl::O_RDWR
-IO.open fd, IO::RDWR{|io|
+IO.open fd, IO::RDWR do |io|
 	io.ioctl(0x0703, 0x48)
 	io.syswrite 0x48.chr
 	buf = io.sysread 12
 	temp = (buf[0].ord << 8 | buf[1].ord) >> 3
 	temp -= 8192 if 4096 <= temp
 	printf "%f\n", temp/16.0
-}
+end
