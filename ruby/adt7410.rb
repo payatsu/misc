a415ce25b2bc808e59e27cb900d2cc93a26c9979
payatsu <6873447+payatsu@users.noreply.github.com>
2016-09-18 21:32:07 +0900

    adt7410.rbで温度計算部分改良した。

--- ruby/adt7410.rb
+++ ruby/adt7410.rb
@@ -5,11 +5,10 @@ if $0 == __FILE__
 	require_relative 'i2c'
 	ADT7410 = 0x48
 	DATA_SIZE = 12
-	buf = i2c_read ADT7410, 0, DATA_SIZE
+	buf = i2c_read ADT7410, 0.chr, DATA_SIZE
 	temp = (buf[0].ord << 8 | buf[1].ord) >> 3
-	temp -= 8192 if 4096 <= temp
+	temp -= 8192 if temp & 0xf000 != 0
 	printf "%f℃\n", temp/16.0
-
 	buf.each_byte do |i|
 		printf '%02x ', i.ord
 	end
