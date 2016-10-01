90d712d69261f7aae90b366da8032055a23a42a4
payatsu <6873447+payatsu@users.noreply.github.com>
2016-10-02 01:08:03 +0900

    doxygen.sh改良。

--- sh/doxygen.sh
+++ sh/doxygen.sh
@@ -5,20 +5,29 @@
 ! which global  > /dev/null 2>&1 && echo $0: command not found: global  >&2 && exit 1
 
 tty=`tty`
-
-while getopts e:i: arg; do
+clang_assisted_parsing=NO
+while getopts cD:e:I: arg; do
 	case ${arg} in
+	c)
+		clang_assisted_parsing=YES
+		;;
+	D)
+		macro_definitions="${macro_definitions} ${OPTARG}"
+		;;
 	e)
 		exclude="${exclude} ${OPTARG}"
 		;;
-	i)
+	I)
 		include="${include} ${OPTARG}"
+		;;
+	\?)
+		exit 1
 		;;
 	esac
 done
 shift `expr ${OPTIND} - 1`
 
-css=doxygen.css
+css_file=my_style.css
 doxygen -w html /dev/null /dev/null - | sed -e '
 s/Roboto/Ricty,&/
 s/monospace/Ricty, &/
@@ -27,7 +36,7 @@ s/Arial/Ricty, &/
 s/,Geneva/,Ricty&/
 s/Tahoma/Ricty,&/
 s/Verdana/Ricty,&/
-' > ${css}
+' > ${css_file}
 
 doxygen -g - | sed -e '
 /^PROJECT_NAME /s/= .\+$/= "{project name}"/
@@ -38,18 +47,21 @@ doxygen -g - | sed -e '
 /^EXTRACT_STATIC /s/= NO$/= YES/
 /^INPUT /s%=$%= '"$*"'%
 /^RECURSIVE /s/= NO$/= YES/
-/^EXCLUDE /s/=$/='"${exclude}"'/
+/^EXCLUDE /s%=$%='"${exclude}"'%
 /^SOURCE_BROWSER /s/= NO$/= YES/
 /^INLINE_SOURCES /s/= NO$/= YES/
 /^REFERENCED_BY_RELATION /s/= NO$/= YES/
 /^REFERENCES_RELATION /s/= NO$/= YES/
 # /^USE_HTAGS /s/= NO$/= YES/
-# CLANG_ASSISTED_PARSING
-/^HTML_EXTRA_STYLESHEET /s/=$/= '${css}'/
+# /^CLANG_ASSISTED_PARSING /s/= NO$/= '${clang_assisted_parsing}'/
+# /^CLANG_OPTIONS /s%=$%= -stdlib=libc++%#'"`echo \"${include}\" | sed -e 's/ / -I/g'`"'%
+/^HTML_EXTRA_STYLESHEET /s%=$%= '${css_file}'%
 /^HTML_TIMESTAMP /s/= NO$/= YES/
 # /^HTML_DYNAMIC_SECTIONS /s/= NO$/= YES/
 /^GENERATE_TREEVIEW /s/= NO$/= YES/
 /^GENERATE_LATEX /s/= YES$/= NO/
+/^INCLUDE_PATH /s%=$%='"${include}"'%
+/^PREDEFINED /s/=$/='"${macro_definitions}"'/
 /^HIDE_UNDOC_RELATIONS /s/= YES$/= NO/
 /^HAVE_DOT /s/= NO$/= YES/
 /^DOT_FONTNAME /s/= [[:alpha:]]\+$/= Ricty/
