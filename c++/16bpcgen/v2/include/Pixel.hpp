9f8850729b9020c4fc739eb914968433f0fa1858
payatsu <6873447+payatsu@users.noreply.github.com>
2016-11-06 21:59:59 +0900

    16bpcgenでYCbCr601/709とRGBの変換機能を追加（レンジの区別は非対応）

--- c++/16bpcgen/v2/include/Pixel.hpp
+++ c++/16bpcgen/v2/include/Pixel.hpp
@@ -10,7 +10,30 @@ class Pixel{
 public:
 	typedef T value_type;
 	const static value_type max;
-	Pixel(value_type r = 0x0, value_type g = 0x0, value_type b = 0x0): R_(r), G_(g), B_(b){}
+	enum ColorSpace{
+		CS_RGB,
+		CS_YCBCR601,
+		CS_YCBCR709
+	};
+	Pixel(value_type r_y = 0x0, value_type g_cb = 0x0, value_type b_cr = 0x0, ColorSpace cs = CS_RGB): R_(r_y), G_(g_cb), B_(b_cr)
+	{
+		switch(cs){
+		case CS_RGB:
+			break;
+		case CS_YCBCR601:
+			R_ = 1.164*(r_y - 16.0*max/255)                                + 1.596*(b_cr - 128.0*max/255);
+			G_ = 1.164*(r_y - 16.0*max/255) - 0.391*(g_cb - 128.0*max/255) - 0.813*(b_cr - 128.0*max/255);
+			B_ = 1.164*(r_y - 16.0*max/255) + 2.018*(g_cb - 128.0*max/255);
+			break;
+		case CS_YCBCR709:
+			R_ = 1.164*(r_y - 16.0*max/255)                                + 1.793*(b_cr - 128.0*max/255);
+			G_ = 1.164*(r_y - 16.0*max/255) - 0.213*(g_cb - 128.0*max/255) - 0.533*(b_cr - 128.0*max/255);
+			B_ = 1.164*(r_y - 16.0*max/255) + 2.112*(g_cb - 128.0*max/255);
+			break;
+		default:
+			throw std::runtime_error(__func__);
+		}
+	}
 	template <typename U>
 	Pixel(const Pixel<U>& rhs):
 		R_(static_cast<value_type>(rhs.R())),
@@ -151,6 +174,32 @@ public:
 	void R(value_type r){R_ = r;}
 	void G(value_type g){G_ = g;}
 	void B(value_type b){B_ = b;}
+	double H()const
+	{
+		const value_type maximum = std::max(std::max(R_, G_), B_);
+		const value_type minimum = std::min(std::min(R_, G_), B_);
+		const double diff = maximum - minimum;
+		if(minimum == maximum){
+			throw std::runtime_error(__func__ + std::string(": Hue undefined."));
+		}else if(minimum == B_){
+			return 60 * (static_cast<double>(G_) - R_) / max / diff + 60;
+		}else if(minimum == R_){
+			return 60 * (static_cast<double>(B_) - G_) / max / diff + 180;
+		}else{
+			return 60 * (static_cast<double>(R_) - B_) / max / diff + 300;
+		}
+	}
+	double S()const
+	{
+		return static_cast<double>(std::max(std::max(R_, G_), B_) - std::min(std::min(R_, G_), B_))/max;
+	}
+	double V()const{return static_cast<double>(std::max(std::max(R_, G_), B_))/max;}
+	value_type  Y601()const{return  0.257*R_ + 0.504*G_ + 0.098*B_ +  16.0*max/255;}
+	value_type Cb601()const{return -0.148*R_ - 0.291*G_ + 0.439*B_ + 128.0*max/255;}
+	value_type Cr601()const{return  0.439*R_ - 0.368*G_ - 0.071*B_ + 128.0*max/255;}
+	value_type  Y709()const{return  0.183*R_ + 0.614*G_ + 0.062*B_ +  16.0*max/255;}
+	value_type Cb709()const{return -0.101*R_ - 0.339*G_ + 0.439*B_ + 128.0*max/255;}
+	value_type Cr709()const{return  0.439*R_ - 0.399*G_ - 0.040*B_ + 128.0*max/255;}
 private:
 	value_type R_;
 	value_type G_;
