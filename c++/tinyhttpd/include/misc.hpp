ec7a489b87f6913976e284c4d98e33c6585c71dc
payatsu <6873447+payatsu@users.noreply.github.com>
2016-12-10 08:31:52 +0900

    tinyhttpdで、POSTで受信した文字列の処理をmisc.cppに仮分離した。

---
+++ c++/tinyhttpd/include/misc.hpp
@@ -0,0 +1,8 @@
+#ifndef TINYHTTPD_MISC_HPP_
+#define TINYHTTPD_MISC_HPP_
+
+#include <string>
+
+void generate_image(const std::string& str);
+
+#endif
--- c++/tinyhttpd/src/Httpd.cpp
+++ c++/tinyhttpd/src/Httpd.cpp
@@ -5,67 +5,7 @@
 #include <iostream>
 #include <sstream>
 #include "Httpd.hpp"
-
-
-
-#include <algorithm>
-#include <iterator>
-#include <vector>
-#include "Image.hpp"
-struct NonDigitEliminator{
-	char operator()(const char c)const
-	{
-		if(('0' <= c && c <= '9') || ('a' <= c && c <= 'f') || ('A' <= c && c <= 'F') || c == '\n'){
-			return c;
-		}else{
-			return ' ';
-		}
-	}
-};
-void generate_image(const std::string& str)
-{
-	std::string buffer;
-	std::transform(str.begin(), str.end(), std::back_inserter(buffer), NonDigitEliminator());
-
-	std::istringstream data(buffer);
-	std::string line;
-	std::vector<std::vector<unsigned int> > values;
-	while(std::getline(data, line)){
-		if(line == ""){
-			continue;
-		}
-		std::istringstream iss(line);
-		iss >> std::hex;
-		values.push_back(std::vector<unsigned int>());
-		std::back_insert_iterator<std::vector<unsigned int> > it(values.back());
-		std::istream_iterator<unsigned int> isi(iss);
-		std::istream_iterator<unsigned int> last;
-		for(; isi != last; ++isi){
-			*it = *isi;
-		}
-	}
-	try{
-		const std::size_t elems = values.at(0).size();
-		for(std::vector<std::vector<unsigned int> >::iterator it = values.begin(); it != values.end(); ++it){
-			if(it->size() != elems){
-				std::cerr << "invalid input." << std::endl;
-				return;
-			}
-		}
-		Image img(values.at(0).size(), values.size()/3);
-		for(std::size_t i = 0; i < values.size(); i += 3){
-			for(std::size_t j = 0; j < values.at(0).size(); ++j){
-				img[i][j].R(values.at(i    ).at(j));
-				img[i][j].G(values.at(i + 1).at(j));
-				img[i][j].B(values.at(i + 2).at(j));
-			}
-		}
-		img >> "./res/result.png";
-	}catch(const std::out_of_range& err){
-		std::cerr <<err.what() << std::endl;
-		return;
-	}
-}
+#include "misc.hpp"
 
 const char Httpd::crlf[] = {0x0d, 0x0a, 0x00};
 const char Httpd::emptyline[] = {0x0d, 0x0a, 0x0d, 0x0a, 0x00};
@@ -235,11 +175,9 @@ void Httpd::get_content(Httpd::Field& field, std::ostringstream& reply)
 
 void Httpd::dump_request(const Field& field)
 {
-	std::cout << "[http request]" << std::endl;
 	for(Httpd::Field::const_iterator it = field.begin(); it != field.end(); ++it){
 		std::cout << it->first << ": " << it->second << std::endl;
 	}
-	std::cout << std::endl;
 }
 
 void Httpd::dump_reply(const std::string& reply)
