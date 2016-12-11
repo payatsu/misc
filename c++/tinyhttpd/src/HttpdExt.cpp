f46337830776fc35df694194001343d7c63e26e7
payatsu <6873447+payatsu@users.noreply.github.com>
2016-12-11 17:05:02 +0900

    tinyhttpdで複数画像を横方向に結合できるようにした。

--- c++/tinyhttpd/src/HttpdExt.cpp
+++ c++/tinyhttpd/src/HttpdExt.cpp
@@ -33,17 +33,25 @@ void HttpdExt::process_post_data(HttpdExt::Field& field)const
 	std::string line;
 	int i = 0;
 	Image result(0, 0);
+	Image tile(0, 0);
+	Image line_buffer0(0, 0);
+	Image line_buffer1(0, 0);
+	Image line_buffer2(0, 0);
 	while(std::getline(data, line)){
+		if(line == ""){
+			result = result(tile, Image::ORI_HORI);
+			tile = Image(0, 0);
+			continue;
+		}
 		std::istringstream iss(line);
 		iss >> std::hex;
 		uint16_t value;
-		Image line_buffer(0, 0);
 		switch(i % 3){
 		case 0:{
 			while(iss >> value){
 				Image tmp(1, 1);
 				tmp[0][0] = Image::pixel_type(value, 0, 0) << 4;
-				line_buffer = line_buffer(tmp, Image::ORI_HORI);
+				line_buffer0 = line_buffer0(tmp, Image::ORI_HORI);
 			}
 			break;
 		}
@@ -51,7 +59,7 @@ void HttpdExt::process_post_data(HttpdExt::Field& field)const
 			while(iss >> value){
 				Image tmp(1, 1);
 				tmp[0][0] = Image::pixel_type(0, value, 0) << 4;
-				line_buffer = line_buffer(tmp, Image::ORI_HORI);
+				line_buffer1 = line_buffer1(tmp, Image::ORI_HORI);
 			}
 			break;
 		}
@@ -59,15 +67,23 @@ void HttpdExt::process_post_data(HttpdExt::Field& field)const
 			while(iss >> value){
 				Image tmp(1, 1);
 				tmp[0][0] = Image::pixel_type(0, 0, value) << 4;
-				line_buffer = line_buffer(tmp, Image::ORI_HORI);
+				line_buffer2 = line_buffer2(tmp, Image::ORI_HORI);
 			}
+			line_buffer0 = line_buffer0 | line_buffer1;
+			line_buffer0 = line_buffer0 | line_buffer2;
+			tile = tile(line_buffer0, Image::ORI_VERT);
+			line_buffer0 = Image(0, 0);
+			line_buffer1 = Image(0, 0);
+			line_buffer2 = Image(0, 0);
 			break;
 		}
 		default:
 			break;
 		}
-		result = result(line_buffer, Image::ORI_VERT);
 		++i;
+	}
+	if(tile.data_size()){
+		result = result(tile, Image::ORI_HORI);
 	}
 	result >> "./res/result.png";
 }
