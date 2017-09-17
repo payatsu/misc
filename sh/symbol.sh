107fff19285380a424eb16cf7eca027c1be67189
payatsu <6873447+payatsu@users.noreply.github.com>
2017-09-17 18:02:27 +0900

    symbol.shのリファクタリング

--- sh/symbol.sh
+++ sh/symbol.sh
@@ -2,22 +2,33 @@
 
 usage()
 {
+	this=`basename ${0}`
 	cat <<-EOF
-	[Options]
+	[NAME]
+	    ${this} - show symbol's entity within ELF file, and overwrite it with data in another binary file.
+
+	[SYNOPSIS]
+	    ${this} -s symbol_name elf_file
+	    ${this} -s symbol_name any_file elf_file
+
+	[OPTIONS]
 	    -h
-	        show this help.
+	        show this help and exit.
+
 	    -s symbol
 	        specify symbol.
 
-	[Examples]
-	    * To show data that symbol "foo" points in bar.elf:
-	        \$ `basename ${0}` -s foo bar.elf
-	    * To overwrite data that symbol "foo" points in bar.elf with baz.bin:
-	        \$ `basename ${0}` -s foo baz.bin bar.elf
+	[EXAMPLES]
+	    * To show data that symbol "foo" points in "bar.elf" with hex dump:
+	        \$ ${this} -s foo bar.elf
+
+	    * To overwrite data that symbol "foo" points in "bar.elf" with baz.bin:
+	        \$ ${this} -s foo baz.bin bar.elf
 
 	EOF
 }
 
+symbol=
 while getopts hs: arg; do
 	case ${arg} in
 	h)  usage; exit;;
@@ -27,10 +38,11 @@ while getopts hs: arg; do
 done
 shift `expr ${OPTIND} - 1`
 
-# $1: ELF file
-# $2: symbol name
+# ${1}: ELF file
+# ${2}: symbol name
 scan()
 {
+	[ -n "${2}" ] || { echo Error. Symbol is not specified. >&2; return 1;}
 	symbol_table=`LANG=C readelf -s -W ${1} | grep -e '^   Num:\|\<'${2}'\>$' | grep -e '\<'${2}'\>$' -B 1`
 	echo "${symbol_table}" | grep -qe ${2} || { echo Error. Symbol \"${2}\" is not found in \"${1}\". >&2; return 1;}
 	symbol_size=`echo "${symbol_table}" | awk 'NR == 2{print $3}'`
@@ -53,43 +65,39 @@ scan()
 		${section_header}
 	EOF
 	`
-	symbol_location=`echo "${offset}" | sed -e 's/^.\+located at \(\<0x[[:xdigit:]]\+\>\).\+$/\1/'`
 	cat <<-EOF
 		${offset}
 
 	EOF
+	symbol_location=`echo "${offset}" | sed -e 's/^.\+located at \(\<0x[[:xdigit:]]\+\>\).\+$/\1/'`
+	echo The hex dump of symbol \"${2}\" in \"${1}\" is as follows:
+	dump ${1} ${symbol_location} ${symbol_size}
 }
 
-# $1: ELF file
+# ${1}: output ELF file
+# ${2}: symbol name
+# ${3}: input binary file
+overwrite()
+{
+	scan ${1} ${symbol} || return
+	[ `wc -c < ${3}` -le ${symbol_size} ] || {return}
+	LANG=C dd if=${3} of=${1} bs=1 count=${symbol_size} seek=`printf '%d' ${symbol_location}` conv=notrunc status=none
+
+	echo And it was overwritten just now as follows:
+	dump ${1} ${symbol_location} ${symbol_size}
+}
+
+# ${1}: ELF file
+# ${2}: symbol location in ELF file
+# ${3}: symbol size in ELF file
 dump()
 {
-	od -Ax -j ${symbol_location} -N ${symbol_size} -tx1z -v ${1} | grep -v '^[[:xdigit:]]\+$'
+	od -Ax -j ${2} -N ${3} -tx1z -v ${1} | grep -v '^[[:xdigit:]]\+$'
 	echo
 }
 
-# $1: output ELF file
-# $2: symbol name
-# $3: input binary file
-overwrite()
-{
-	echo The hex dump of symbol \"${2}\" in \"${1}\" is as follows:
-	dump ${1}
-	
-	[ `wc -c < ${3}` -le ${symbol_size} ] || {return}
-	LANG=C dd if=${3} of=${1} bs=1 count=${symbol_size} seek=`printf '%d' ${symbol_location}` conv=notrunc status=none
-	
-	echo And it was overwritten just now as follows:
-	dump ${1}
-}
-
 case $# in
-	0);;
-	1)
-		scan ${1} ${symbol} || exit
-		dump ${1};;
-	2)
-		scan ${2} ${symbol} || exit
-		overwrite ${2} ${symbol} ${1};;
-	*);;
+	1) scan ${1} ${symbol};;
+	2) overwrite ${2} ${symbol} ${1};;
+	*) usage;;
 esac
-
