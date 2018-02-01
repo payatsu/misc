62ea874ce7a9db87cdb70c8aca9dbf6a43ebedf2
payatsu <6873447+payatsu@users.noreply.github.com>
2018-02-01 23:59:59 +0900

    Add characters in fps.sh

--- sh/fps.sh
+++ sh/fps.sh
@@ -2,17 +2,14 @@
 
 width=1920
 height=1080
-
 n=4
 
-tmpdir=.
-# tmpdir=`mktemp --tmpdir -d`
-# trap 'rm -rf ${tmpdir}' EXIT HUP INT QUIT TERM
+tmpdir=`mktemp --tmpdir -d`
+trap 'rm -rf ${tmpdir}' EXIT HUP INT QUIT TERM
 
 rectangle_width=`expr ${width} / ${n}`
 rectangle_height=`expr ${height} / ${n}`
-
-convert -size ${width}x${height} xc:white -fill black \
+convert -verbose -size ${width}x${height} xc:white -fill black \
 	-draw "rectangle `expr ${rectangle_width} \* 0`,`expr ${rectangle_height} \* 0` `expr ${rectangle_width} \* 1`,`expr ${rectangle_height} \* 1`" \
 	-draw "rectangle `expr ${rectangle_width} \* 2`,`expr ${rectangle_height} \* 0` `expr ${rectangle_width} \* 3`,`expr ${rectangle_height} \* 1`" \
 	-draw "rectangle `expr ${rectangle_width} \* 1`,`expr ${rectangle_height} \* 1` `expr ${rectangle_width} \* 2`,`expr ${rectangle_height} \* 2`" \
@@ -21,13 +18,11 @@ convert -size ${width}x${height} xc:white -fill black \
 	-draw "rectangle `expr ${rectangle_width} \* 2`,`expr ${rectangle_height} \* 2` `expr ${rectangle_width} \* 3`,`expr ${rectangle_height} \* 3`" \
 	-draw "rectangle `expr ${rectangle_width} \* 1`,`expr ${rectangle_height} \* 3` `expr ${rectangle_width} \* 2`,`expr ${rectangle_height} \* 4`" \
 	-draw "rectangle `expr ${rectangle_width} \* 3`,`expr ${rectangle_height} \* 3` `expr ${rectangle_width} \* 4`,`expr ${rectangle_height} \* 4`" \
-	${tmpdir}/base.png
+	${tmpdir}/base.png || return
 
-image_name_format=img_%06d
-
-for i in `seq ${rectangle_height}`; do
-	name=`printf ${image_name_format} ${i}`
-	convert ${tmpdir}/base.png -roll +${i}+${i} ${name}.png
+fmt=%05d
+for i in `seq \`expr ${rectangle_height} \* 1\``; do
+	num=`printf ${fmt} ${i}`
+	convert -verbose ${tmpdir}/base.png -roll +${i}+${i} -fill red -font Ricty-Regular -pointsize 400 -gravity center -annotate 0 ${num} ${tmpdir}/img_${num}.png || return
 done
-
-ffmpeg -framerate 120 -i ${image_name_format}.png -vcodec h264 -pix_fmt yuv444p -an -qscale 0 -r 120 movie.mp4
+ffmpeg -framerate 120 -i ${tmpdir}/img_${fmt}.png -vcodec h264 -pix_fmt yuv420p -an -q:v 0 -r 120 movie.mp4 || return
